8ab63ab17f4f99d363530427219b84ebf058c91b
EbyvanBokkem <9018376@student.zadkine.nl>
2022-09-15 14:56:55 +0200

    javascript

--- oefenen/app.js
+++ oefenen/app.js
@@ -7,3 +7,28 @@ document.write(myName)
 document.write(myAge)
 document.write(diploma)
 
+let a = 5 * 3;
+
+document.write(a)
+
+let num1 = 69;
+let num2 = 666; 
+let answer = num1 + num2;
+
+console.log(answer)
+
+
+let answer1 = num1 - num2;
+
+console.log(answer1)
+
+let answer2 = num1 * num2;
+
+console.log(answer2)
+
+let answer3 = num1 / num2;
+
+console.log(answer3)
+
+
+
