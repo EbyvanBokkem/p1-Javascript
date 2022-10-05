c095b24fae96901067861d05906ddf55eb37583d
EbyvanBokkem <9018376@student.zadkine.nl>
2022-10-05 08:43:38 +0200

    calculator

---
+++ opdracht_6.4/app.js
@@ -0,0 +1,11 @@
+// let num1 = 96;
+// let num2 = 204;
+// let anwser = num1 + num2;
+
+// console.log(anwser)
+
+function calculate(num1, num2){
+    let anwser = num1 + num2;
+    console.log(anwser)
+}
+calculate(69,69)
