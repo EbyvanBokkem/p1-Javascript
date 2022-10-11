bb79d60f09895c90d8edafd359ae07caaa8a79d7
EbyvanBokkem <9018376@student.zadkine.nl>
2022-10-11 12:00:51 +0200

    7.2

--- opdracht_7.1/app.js
+++ opdracht_7.1/app.js
@@ -1,9 +1,23 @@
-let budget = 100;
-let product = 60;
+// begin 7.1
+// let budget = 100;
+// let product = 60;
 
-if(budget >product){
-    console.log("U heeft genoeg geld!")
-} 
-else if(budget <product) {
-    console.log("Helaas, te weinig geld")
+// if(budget >product){
+//     console.log("U heeft genoeg geld!")
+// } 
+// else if(budget <product) {
+//     console.log("Helaas, te weinig geld")
+// }
+// eind 7.1
+
+//  begin 7.2
+let budget = prompt("budget", "");  
+let product = 60
+if (budget > product) {
+    document.getElementById("eindbedrag").style.color = "green"
+    document.getElementById("eindbedrag").innerHTML = "U heeft genoeg geld!"
+} else {
+    document.getElementById("eindbedrag").style.color = "red"
+    document.getElementById("eindbedrag").innerHTML = "Helaas, te weinig geld"
 }
+// eind 7.2
